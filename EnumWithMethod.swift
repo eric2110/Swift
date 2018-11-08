39807e758eeadc56a7edf6da9f0c6c573abae8a0
eric2110 <eric_liu2110@hotmail.com>
2018-11-08 09:46:27 +0800

    20181108 enum

---
+++ EnumWithMethod.swift
@@ -0,0 +1,31 @@
+import Foundation 
+
+enum Lightbulb {
+    case on
+    case off
+        func surfaceTemperature(forAmbientTemperature ambient: Double) -> Double {
+        switch self {
+            case .on:
+            return ambient + 150.0
+            case .off:
+            return ambient
+        }
+    }
+    
+    mutating func toggle() {
+        switch self {
+            case .on:
+            self = .off
+            case .off:
+            self = .on
+        }
+    }
+}
+
+var bulb = Lightbulb.on
+let ambientTemperature = 77.0
+var bulbTemperature = bulb.surfaceTemperature(forAmbientTemperature:ambientTemperature)
+print("the bulb's temperature is \(bulbTemperature)")
+bulb.toggle()
+bulbTemperature = bulb.surfaceTemperature(forAmbientTemperature: ambientTemperature)
+print("the bulb's temperature is \(bulbTemperature)")
